901a7da348e32f204d929fc032dd13a257cabeb7
athms <athms.research@gmail.com>
2023-02-06 12:47:38 -0800

    increase time request to 10h

--- scripts/sbatch/hyperopt.sh
+++ scripts/sbatch/hyperopt.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1                   
 #SBATCH -n 1                   
 #SBATCH -p gtx                 
-#SBATCH -t 05:00:00            
+#SBATCH -t 10:00:00            
 
 while [ $# -gt 0 ] ; do
   case $1 in
